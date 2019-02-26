5d59ba54665e9317a07086edf140063a741f5789
qtsidekick <qtsidekick@gmail.com>
2019-02-26 01:21:51 -0600

    Added more exercise files

---
+++ Crash_Course/2.3-2.7/2-3_personal_message.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_name = "Kevin"
+greeting_message = "Hello"
+question_one = "Would you like to learn Python today?"
+
+print(greeting_message + "," + " " + user_name + "." + " " + question_one)
---
+++ Crash_Course/2.3-2.7/2-4_name_cases.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+full_name = "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff"
+
+print("(1) lowercase - " + full_name.lower())
+print("(2) UPPERCASE - " + full_name.upper())
+print("(3) title - " + full_name.title())
