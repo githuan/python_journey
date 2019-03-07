ba69361147fb4af583ea5d62bdac27329e9fd7f8
Thuan Nguyen <qtsidekick@gmail.com>
2019-03-07 00:04:29 -0600

    Update files again

--- Crash_Course/4.10-4.12/4-12_more_loops.py
+++ Crash_Course/4.10-4.12/4-12_more_loops.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
 
+#!/usr/bin/env python3
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+fiance_foods = my_foods[-3:]
+
+print("My favorite foods are:")
+#print(my_foods)
+for food in my_foods:
+    print(food.title())
+
+print("\nMy friend's favorite foods are:")
+#print(friend_foods)
+for food in friend_foods:
+    print(food.title())
+
+print("\nMy fiance's favorite foods are:")
+#print(fiance_foods)
+for food in fiance_foods:
+    print(food.title())
+
--- Crash_Course/extras/foods.py
+++ Crash_Course/extras/foods.py
@@ -5,17 +5,10 @@ friend_foods = my_foods[:]
 fiance_foods = my_foods[-3:]
 
 print("My favorite foods are:")
-#print(my_foods)
-for food in my_foods:
-    print(food.title())
+print(my_foods)
 
 print("\nMy friend's favorite foods are:")
-#print(friend_foods)
-for food in friend_foods:
-    print(food.title())
+print(friend_foods)
 
 print("\nMy fiance's favorite foods are:")
-#print(fiance_foods)
-for food in fiance_foods:
-    print(food.title())
-
+print(fiance_foods)
