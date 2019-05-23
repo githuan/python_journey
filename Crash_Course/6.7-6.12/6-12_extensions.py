2b4de2f0a2d3ddedb8670cf50e46c680f4c6351a
Thuan Nguyen <qtsidekick@gmail.com>
2019-05-23 16:35:45 -0500

    Update file 6-12

--- Crash_Course/6.7-6.12/6-12_extensions.py
+++ Crash_Course/6.7-6.12/6-12_extensions.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
 
-
-#!/usr/bin/env python3
-
 # Define favorite languages
-fav_lang = {
-    'fred':'c',
-    'janna':'java',
-    'link':'lua',
-    'mark':'c#',
-    'john':'assembly',
-    'thuan':'python',
-    'selena':'java',
-    'thuan':'c',
-    'komos':'ruby',
-    'vicktor':'c++',
+favorite_languages = {
+    'fred': ['c', 'c#'],
+    'janna': ['java', 'javascript', 'node.js'],
+    'link': ['lua', 'python'],
+    'mark': ['c#', 'python', 'c', 'c++', 'haskell'],
+    'john': ['assembly', 'haskell', 'c'],
+    'thuan': ['python', 'go'],
+    'selena': ['java', 'javascript'],
+    'thuan': ['c', 'c++'],
+    'komos': ['ruby', 'python', 'php'],
+    'vicktor': ['c++', 'php'],
     }
 
 # Define friends list
@@ -24,13 +21,19 @@ friends = [
     'benjamin', 'thuan', 'selena', 'vicktor'
     ]
 
-for name,lang in fav_lang.items():
+for name, languages in favorite_languages.items():
     if name in friends:
-        print("Hi " + name.title() +
-              ", I see your favorite language is " +
-              lang.title() + "!\n"
-              )
+        print("\nHi " + name.title() + ", your favorite languages are:")
+        for language in sorted(languages):
+            if language == 'javascript':
+                print("\tJavaScript")
+            elif language == 'node.js':
+                print("\tNode.JS")
+            elif language == 'php':
+                print("\tPHP")
+            else:
+                print("\t" + language.title())
     
 for friend in friends:
-    if friend not in fav_lang.keys():
+    if friend not in favorite_languages.keys():
         print(friend.title() + ", please take our poll.\n")
