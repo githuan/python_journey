063a2f26288d7db2ce7ede2044df1272c140a8db
Thuan Nguyen <qtsidekick@gmail.com>
2019-06-06 22:49:15 -0500

    Add chapter 8 files

---
+++ Crash_Course/8.1-8.2/8-1_message.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def display_message():
+    """Display the purpose of function in Python 3"""
+    print("In chapter 8, you will learn ways to pass information to functions.")
+
+
+display_message()
---
+++ Crash_Course/8.1-8.2/8-2_favorite_book.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def favorite_book(title):
+    """Display book title"""
+    print(f"One of my favorite book is {title.title()}.")
+
+
+favorite_book('Games of Thrones')
